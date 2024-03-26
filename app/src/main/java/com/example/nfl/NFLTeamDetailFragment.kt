bb0b60e0363be72f680e4b0823ec3a4ec4c99257
kshitija <ksjadhav@iu.edu>
2024-03-25 22:40:22 -0400

    NFL V5 completed

--- app/src/main/java/com/example/nfl/NFLTeamDetailFragment.kt
+++ app/src/main/java/com/example/nfl/NFLTeamDetailFragment.kt
@@ -13,17 +13,26 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.navArgs
 import com.example.nfl.databinding.FragmentTeamDetailBinding
 import kotlinx.coroutines.launch
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 private const val TAG = "NFLTeamDetailsFragment"
 private lateinit var nflTeam: NFLTeam
 private lateinit var binding : FragmentTeamDetailBinding
 
-class NFLTeamDetailFragment: Fragment() {
+class NFLTeamDetailFragment: Fragment(), OnMapReadyCallback {
    // private lateinit var binding: FragmentNflBinding
 //    private var _binding: FragmentTeamDetailBinding? = null
     private val args : NFLTeamDetailFragmentArgs by navArgs()
 
     private lateinit var data: NFLTeam
+
+    private lateinit var map: GoogleMap
+
+    private var nflTeam: NFLTeam? = null // Change this line
 
     private val teamDetailVM : TeamDetailVM by viewModels {
         TeamDetailVMFactory(args.teamId)
@@ -35,16 +44,22 @@ class NFLTeamDetailFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentTeamDetailBinding.inflate(inflater, container, false)
+        binding.mapView.onCreate(savedInstanceState)
+
         return binding.root
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewLifecycleOwner.lifecycleScope.launch{
-            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED){
-                teamDetailVM.team.collect{ team ->
-                    team?.let { updateUi(it) }
+        binding.mapView.getMapAsync(this)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                teamDetailVM.team.collect { team ->
+                    team?.let {
+                        nflTeam = it // Set the nflTeam variable
+                        updateUi(it)
+                    }
                 }
             }
         }
@@ -60,6 +75,23 @@ class NFLTeamDetailFragment: Fragment() {
             val logoResourceId = resources.getIdentifier(resourceName.toLowerCase(), "drawable", context?.packageName)
             imageView.setImageResource(logoResourceId)
         }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.uiSettings.isZoomControlsEnabled = false
+        map.uiSettings.isMyLocationButtonEnabled = false
+
+        nflTeam?.let { team ->
+            val nflTeamLocDisp = LatLng(team.latitude, team.longitude)
+            map.addMarker(
+                MarkerOptions()
+                    .position(nflTeamLocDisp)
+                    .visible(true)
+            )
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(nflTeamLocDisp, 15f))
+        }
+        binding.mapView.onResume()
     }
 
 
